f2d9edc1bcc7ac582c355991cd2b1c9e99711022
migueleaferreira <guelexpedito4@gmail.com>
2023-07-28 11:48:33 -0300

    projeto sistema padaria

---
+++ PadariaCarmel/frmLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PadariaCarmel
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem-Vindo ao sistema.",
+                "Mensagem do sistem",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button3);
+        }
+    }
+}
